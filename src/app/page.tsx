a97a04195b3a19ca406bcbb87106aa500807fc24
Krishna Thakkar <work.krishnat@gmail.com>
2024-07-11 23:56:38 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-5xl text-center">hello</h1>
+    <h1 className="text-5xl text-center">tailwin hceck</h1>
   );
 }
